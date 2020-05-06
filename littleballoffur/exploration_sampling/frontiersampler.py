e3cb101e63da2262c8c2e3452f44a6ac63805610
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-05-06 14:19:15 +0000

    Frontier

--- littleballoffur/exploration_sampling/frontiersampler.py
+++ littleballoffur/exploration_sampling/frontiersampler.py
@@ -2,15 +2,17 @@ import random
 import networkx as nx
 from littleballoffur.sampler import Sampler
 
-class RandomNodeNeighborSampler(Sampler):
-    r"""An implementation of random node-neighbor sampling.
+class FrontierSampler(Sampler):
+    r"""An implementation of frontier sampling.
 
     Args:
         number_of_nodes (int): Number of nodes. Default is 100.
+        number_of_seeds (int): Number of seed nodes. Default is 10.
         seed (int): Random seed. Default is 42.
     """
     def __init__(self, number_of_nodes=100, seed=42):
         self.number_of_nodes = number_of_nodes
+        self.numer_of_seeds = number_of_seeds
         self.seed = seed
         self._set_seed()
 
