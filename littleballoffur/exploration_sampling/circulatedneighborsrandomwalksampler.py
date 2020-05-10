9396d0ee17f3fdbafd8d37b4dae7454b5a21dbfe
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-05-10 19:54:06 +0000

    CommunityStructure

--- littleballoffur/exploration_sampling/circulatedneighborsrandomwalksampler.py
+++ littleballoffur/exploration_sampling/circulatedneighborsrandomwalksampler.py
@@ -21,6 +21,31 @@ class CirculatedNeighborsRandomWalkSampler(Sampler):
         Choosing a seed node.
         """
         self._sampled_nodes = set()
+        self._current_node = random.choice(range(self._graph.number_of_nodes()))
+        self._sampled_nodes.add(self._current_node)
+
+
+    def _do_shuffling(self, node):
+        """
+        Shuffling the neighbors of a node in the circulated map.
+
+        Arg types:
+            * **node** *(int)* - The node considered.
+        """
+        self._circulated_map[node] = [neighbor for neighbor in self._graph.neighbors(node)]
+        random.shuffle(self._circulated_map[node])
+
+    def _create_circulated_map(self):
+        self._circulated_map = {}
+        for node in self._graph.nodes():
+            self._do_shuffling(node)
+
+    def _make_a_step(self):
+        if len(self._circulated_map[self._current_node]) == 0:
+            self._do_shuffling(self._current_node)
+        self._current_node = self._circulated_map[self._current_node].pop()
+        self._sampled_nodes.add(self._current_node)
+
 
     def sample(self, graph):
         """
@@ -36,5 +61,8 @@ class CirculatedNeighborsRandomWalkSampler(Sampler):
         self._check_number_of_nodes(graph)
         self._graph = graph
         self._create_node_set()
-             
+        self._create_circulated_map()
+        while len(self._sampled_nodes) < self.number_of_nodes:
+             self._make_a_step()
+        new_graph = graph.subgraph(self._sampled_nodes)
         return new_graph
