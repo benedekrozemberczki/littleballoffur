3f796437ea1fa7336164348adf0a71382bd24482
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-09 23:14:35 +0000

    Base class test

---
+++ test/reader_test.py
@@ -0,0 +1,14 @@
+import networkx as nx
+from littleballoffur.dataset import GraphReader
+
+
+def test_reader():
+    """
+    Testing the graph reading on the Facebook dataset.
+    """
+    reader = GraphReader("facebook")
+
+    graph = reader.get_graph()
+    print(nx.number_of_nodes(graph))
+    print(nx.number_of_edges(graph))
+    assert True==True
