4bf6e8f12029d691de73d791a5fa6e403d13c58f
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-09 22:26:01 +0000

    Travis

---
+++ test/sampler_test.py
@@ -0,0 +1,29 @@
+import networkx as nx
+
+from littleballoffur import Sampler
+
+class MetaSampler(Sampler):
+      """
+      Meta sampler class.
+      """
+      def __init__(self, seed):
+          self.seed = seed
+
+      def fit(self, graph):
+          """
+          Returning the original graph.
+          """
+          self._set_seed()
+          self._check_graph()
+          return graph
+
+def test_sampler():
+    """
+    Testing the sampler base class.
+    """
+    sampler = MetaSampler()
+    new_graph = sampler.sample(graph)
+
+    assert sampler.seed = 42
+    
+
