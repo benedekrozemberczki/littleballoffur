ee61aad0bc6727a9db12c69e15f41337ac764258
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-09 23:39:48 +0000

    Base class test

--- test/exploration_sampler_test.py
+++ test/exploration_sampler_test.py
@@ -1,11 +1,17 @@
 import networkx as nx
 
-#from littleballoffur.exploration_sampling import CommunityStructureExpansionSampler, CirculatedNeighborsRandomWalkSampler
 
 from littleballoffur.exploration_sampling import LoopErasedRandomWalkSampler, BreadthFirstSearchSampler, DepthFirstSearchSampler
-#from littleballoffur.exploration_sampling import RandomWalkSampler, RandomNodeNeighborSampler, MetropolisHastingsRandomWalkSampler
-#from littleballoffur.exploration_sampling import ShortestPathSampler, CommonNeighborAwareRandomWalkSampler, NonBackTrackingRandomWalkSampler
-#from littleballoffur.exploration_sampling import RandomWalkWithRestartSampler, RandomWalkWithJumpSampler, FrontierSampler, ForestFireSampler, SnowBallSampler
+
+from littleballoffur.exploration_sampling import CommunityStructureExpansionSampler, CirculatedNeighborsRandomWalkSampler, SnowBallSampler
+from littleballoffur.exploration_sampling import RandomWalkSampler, MetropolisHastingsRandomWalkSampler, CommonNeighborAwareRandomWalkSampler
+from littleballoffur.exploration_sampling import NonBackTrackingRandomWalkSampler, RandomWalkWithRestartSampler, ForestFireSampler
+
+from littleballoffur.exploration_sampling import ShortestPathSampler, RandomWalkWithJumpSampler, FrontierSampler, RandomNodeNeighborSampler
+
+#-----------------------------------#
+# TESTS FOR SPANNING TREE SAMPLERS. #
+#-----------------------------------#
 
 
 def test_loop_erased_random_walk_sampler():
@@ -47,5 +53,48 @@ def test_depth_first_search_sampler():
     new_graph = sampler.sample(graph)
 
     assert new_graph.number_of_edges()+1 == new_graph.number_of_nodes()
+    assert sampler.number_of_nodes == new_graph.number_of_nodes()
+    assert nx.is_connected(new_graph)
+
+#----------------------------------------#
+# TESTS FOR CONNECTED SUBGRAPH SAMPLERS. #
+#----------------------------------------#
+
+def test_community_structure_expansion_sampler():
+    """
+    Testing the number of nodes and the connectivity.
+    """
+    sampler = CommunityStructureExpansionSampler()
+
+    graph = nx.watts_strogatz_graph(200, 10, 0)
+
+    new_graph = sampler.sample(graph)
+
+    assert sampler.number_of_nodes == new_graph.number_of_nodes()
+    assert nx.is_connected(new_graph)
+
+def test_circulated_neighbors_random_walk_sampler():
+    """
+    Testing the number of nodes and the connectivity.
+    """
+    sampler = CirculatedNeighborsRandomWalkSampler()
+
+    graph = nx.watts_strogatz_graph(200, 10, 0)
+
+    new_graph = sampler.sample(graph)
+
+    assert sampler.number_of_nodes == new_graph.number_of_nodes()
+    assert nx.is_connected(new_graph)
+
+def test_snowball_sampler():
+    """
+    Testing the number of nodes and the connectivity.
+    """
+    sampler = SnowBallSampler()
+
+    graph = nx.watts_strogatz_graph(200, 10, 0)
+
+    new_graph = sampler.sample(graph)
+
     assert sampler.number_of_nodes == new_graph.number_of_nodes()
     assert nx.is_connected(new_graph)
