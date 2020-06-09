75cda38a1c3c0510e9c0b43d2252dbc1cb84de63
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-09 22:04:11 +0000

    Travis

--- test/node_sampler/node_sampler_test.py
+++ test/node_sampler/node_sampler_test.py
@@ -2,18 +2,14 @@ import networkx as nx
 
 from littleballoffur import RandomNodeSampler, DegreeBasedSampler, PageRankBasedSampler
 
-def node_sampler_test(sampler):
+def node_sampler_test():
     """
     Testing the size.
     """
+    sampler = RandomNodeSampler()
+
     graph = nx.watts_strogatz_graph(200, 10, 0)
 
     sub_graph = sampler.sample(graph)
 
     assert sub_graph.number_of_nodes() == sampler.number_of_nodes
-
-node_sampler_test(RandomNodeSampler())
-
-node_sampler_test(DegreeBasedSampler())
-
-node_sampler_test(PageRankBasedSampler())
