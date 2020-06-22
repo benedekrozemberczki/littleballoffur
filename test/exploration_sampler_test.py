b805b91b29ab41e7759659388aea17006b4115c0
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-22 00:50:32 +0000

    LBF NX type test

--- test/exploration_sampler_test.py
+++ test/exploration_sampler_test.py
@@ -150,6 +150,7 @@ def test_common_neighbor_aware_random_walk_sampler():
 
     assert sampler.number_of_nodes == new_graph.number_of_nodes()
     assert nx.is_connected(new_graph)
+    assert type(new_graph) == nx.classes.graph.Graph
 
 
 def test_non_back_trackin_random_walk_sampler():
