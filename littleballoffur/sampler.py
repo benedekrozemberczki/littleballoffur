3cca83cc9c33d876d5ba85563783fe272e680b5a
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-05 19:55:55 +0100

    Assertion fixes

--- littleballoffur/sampler.py
+++ littleballoffur/sampler.py
@@ -26,8 +26,7 @@ class Sampler(object):
     def _check_connectivity(self, graph):
         """Checking the connected nature of a single graph."""
         connected = nx.is_connected(graph)
-        if not connected:
-            raise ValueError("Graph is not connected. Please see requirements.")
+        assert connected, "Graph is not connected."
 
     def _check_directedness(self, graph):
         """Checking the undirected nature of a single graph."""
