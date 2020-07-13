5f30a17a40dd3ea8c9233a7ad7858e1c55282b81
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-13 21:24:36 +0100

    New test

--- test/edge_sampler_test.py
+++ test/edge_sampler_test.py
@@ -16,6 +16,16 @@ def test_random_edge_sampler():
     assert sampler.number_of_edges == new_graph.number_of_edges()
     assert type(new_graph) == nx.classes.graph.Graph
 
+    sampler = RandomEdgeSampler(number_of_edges=25)
+
+    graph = nx.watts_strogatz_graph(100, 10, 0)
+
+    new_graph = sampler.sample(graph)
+
+    assert sampler.number_of_edges == new_graph.number_of_edges()
+    assert type(new_graph) == nx.classes.graph.Graph
+
+
 def test_random_nonde_edge_sampler():
     """
     Testing the edge retention rate.
@@ -28,6 +38,16 @@ def test_random_nonde_edge_sampler():
 
     assert sampler.number_of_edges == new_graph.number_of_edges()
     assert type(new_graph) == nx.classes.graph.Graph
+
+    sampler = RandomNodeEdgeSampler(number_of_edges=25)
+
+    graph = nx.watts_strogatz_graph(100, 10, 0)
+
+    new_graph = sampler.sample(graph)
+
+    assert sampler.number_of_edges == new_graph.number_of_edges()
+    assert type(new_graph) == nx.classes.graph.Graph
+
 
 def test_hybrid_node_edge_sampler():
     """
