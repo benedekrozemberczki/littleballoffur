449ca4a3ad9bdba5463e07521eb650df0015be2d
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-13 21:33:38 +0100

    New test

--- test/exploration_sampler_test.py
+++ test/exploration_sampler_test.py
@@ -138,6 +138,16 @@ def test_circulated_neighbors_random_walk_sampler():
     assert nx.is_connected(new_graph)
     assert type(new_graph) == nx.classes.graph.Graph
 
+    sampler = CirculatedNeighborsRandomWalkSampler(number_of_nodes=25)
+
+    graph = nx.watts_strogatz_graph(100, 2, 0)
+
+    new_graph = sampler.sample(graph)
+
+    assert sampler.number_of_nodes == new_graph.number_of_nodes()
+    assert nx.is_connected(new_graph)
+    assert type(new_graph) == nx.classes.graph.Graph
+
 
 def test_snowball_sampler():
     """
