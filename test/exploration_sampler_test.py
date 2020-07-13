f66da8e57e567bbfd5fbe82ff2d089c341ee1e91
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-13 21:34:41 +0100

    New test

--- test/exploration_sampler_test.py
+++ test/exploration_sampler_test.py
@@ -188,6 +188,16 @@ def test_random_walk_sampler():
     assert nx.is_connected(new_graph)
     assert type(new_graph) == nx.classes.graph.Graph
 
+    sampler = RandomWalkSampler(number_of_nodes=25)
+
+    graph = nx.watts_strogatz_graph(100, 10, 0)
+
+    new_graph = sampler.sample(graph)
+
+    assert sampler.number_of_nodes == new_graph.number_of_nodes()
+    assert nx.is_connected(new_graph)
+    assert type(new_graph) == nx.classes.graph.Graph
+
 
 def test_metropolis_hastings_random_walk_sampler():
     """
