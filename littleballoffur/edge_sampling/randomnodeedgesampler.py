f98fecbb6ccd2570ceb12293d2d5290e156a46a3
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-18 23:11:06 +0100

    Type hints

--- littleballoffur/edge_sampling/randomnodeedgesampler.py
+++ littleballoffur/edge_sampling/randomnodeedgesampler.py
@@ -29,7 +29,7 @@ class RandomNodeEdgeSampler(Sampler):
             self._sampled_edges.add(edge)
 
 
-    def sample(self, graph):
+    def sample(self, graph: nx.classes.graph.Graph):
         """
         Sampling edges randomly from randomly sampled nodes.
 
