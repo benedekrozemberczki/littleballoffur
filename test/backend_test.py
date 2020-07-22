7fb6a930a35b3c43b6709c5dff49441b7d889d8c
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-22 23:49:06 +0100

    Start Backend Based Model.

--- test/backend_test.py
+++ test/backend_test.py
@@ -1,5 +1,11 @@
 from littleballoffur import NetworKitBackEnd, NetworkXBackEnd
 
+def test_networkit_backend():
+    backend = NetworKitBackEnd()
+    x = backend.get_x()
+    assert x == 1
 
-
-
+def test_networkx_backend():
+    backend = NetworXBackEnd()
+    x = backend.get_x()
+    assert x == 1
