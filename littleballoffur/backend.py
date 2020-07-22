d8da159720aa5bee7729b15497212cc8c04e869a
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-23 00:30:27 +0100

    Start Backend Based Model.

--- littleballoffur/backend.py
+++ littleballoffur/backend.py
@@ -12,13 +12,13 @@ class NetworKitBackEnd(object):
     def __init__(self):
         self.x = 1
 
-    def get_number_of_nodes(self, graph: NKGraph):
+    def get_number_of_nodes(self, graph: NKGraph) -> int:
         """
         Given a graph return the number of nodes.
         """
         return graph.numberOfNodes()
 
-    def get_number_of_edges(self, graph: NKGraph):
+    def get_number_of_edges(self, graph: NKGraph) -> int:
         """
         Given a graph return the number of nodes.
         """
