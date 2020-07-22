f34ae65bb4a668b986bcf4a5ddaab05f9ed734e6
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-23 00:32:56 +0100

    Start Backend Based Model.

--- test/backend_test.py
+++ test/backend_test.py
@@ -18,6 +18,5 @@ def test_networkx_backend():
     graph = nx.watts_strogatz_graph(1000, 10, 0.5)
 
     assert 1000 == backend.get_number_of_nodes(graph)
-
-    assert 10000 == backend.get_number_of_edges(graph)
+    assert 5000 == backend.get_number_of_edges(graph)
 
