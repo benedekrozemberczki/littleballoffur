9e6f709e2481fbffccb400a63d2b850c01c3da17
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-22 23:44:58 +0100

    Start Backend Based Model.

--- littleballoffur/backend.py
+++ littleballoffur/backend.py
@@ -3,12 +3,15 @@ import networkit as nk
 
 
 class NetworKitBackEnd(object):
-
+    """
+    Binding the NetworKit backend to serve class methods.
+    """
     def __init__(self):
         self.x = 1
 
 class NetworkXBackEnd(object):
-
-
+    """
+    Binding the NetworkX backend to serve class methods.
+    """
     def __init__(self):
         self.x = 1
