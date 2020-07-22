5e2b21c944f63ae4d7dbe20c50d1662b9383251c
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-22 23:45:52 +0100

    Start Backend Based Model.

--- littleballoffur/__init__.py
+++ littleballoffur/__init__.py
@@ -1,6 +1,7 @@
 from littleballoffur.dataset import *
 
 from littleballoffur.sampler import Sampler
+from littleballoffur.backend import NetworKitBackEnd, NetworkXBackEnd
 from littleballoffur.node_sampling import *
 from littleballoffur.edge_sampling import *
 from littleballoffur.exploration_sampling import *
