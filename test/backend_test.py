222b207cf23a5d53741d5da5efd85057f997a092
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-23 00:24:16 +0100

    Start Backend Based Model.

--- test/backend_test.py
+++ test/backend_test.py
@@ -1,11 +1,19 @@
+import networkx as nx
+import networkit as nk
 from littleballoffur import NetworKitBackEnd, NetworkXBackEnd
 
 def test_networkit_backend():
+
     backend = NetworKitBackEnd()
-    x = backend.get_x()
-    assert x == 1
+
+    graph = nk.generators.WattsStrogatzGenerator(1000, 10, 0.5)
+
+    assert 1000 == backend.get_number_of_nodes(graph)
 
 def test_networkx_backend():
+
     backend = NetworkXBackEnd()
-    x = backend.get_x()
-    assert x == 1
+
+    graph = nx.watts_strogatz_graph(1000, 10, 0.5)
+
+    assert 1000 == backend.get_number_of_nodes(graph)
