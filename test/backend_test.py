3accb0ebb711c413793659beeae26358ded8a412
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-25 19:58:29 +0100

    Backend development - neighbors.

--- test/backend_test.py
+++ test/backend_test.py
@@ -34,6 +34,8 @@ def test_networkit_backend():
     assert random_neighbor in [0, 2]
     assert random_neighbor not in [3, 4, 5]
 
+    print(nk.distance.ReverseBFS(G=graph, source=0, target=5))
+
 
 def test_networkx_backend():
 
