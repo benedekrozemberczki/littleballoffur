b9b05204c400cf1c87d50c96bddcff27223e03e8
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-29 21:56:24 +0000

    Do

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,9 +8,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
