f57ab33384db85ca5365077c7baa2be102038295
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-09 23:08:19 +0000

    Backend dev

--- littleballoffur/exploration_sampling/forestfiresampler.py
+++ littleballoffur/exploration_sampling/forestfiresampler.py
@@ -1,8 +1,14 @@
 import random
 import numpy as np
 import networkx as nx
+import networkit as nk
+from typing import Union
 from collections import deque
 from littleballoffur.sampler import Sampler
+
+
+NKGraph = type(nk.graph.Graph())
+NXGraph = nx.classes.graph.Graph
 
 class ForestFireSampler(Sampler):
     r"""An implementation of forest fire sampling. The procedure is a stochastic
@@ -54,16 +60,15 @@ class ForestFireSampler(Sampler):
         Sampling nodes iteratively with a forest fire sampler.
 
         Arg types:
-            * **graph** *(NetworkX graph)* - The graph to be sampled from.
+            * **graph** *(NetworkX or NetworKit graph)* - The graph to be sampled from.
 
         Return types:
-            * **new_graph** *(NetworkX graph)* - The graph of sampled nodes.
+            * **new_graph** *(NetworkX or NetworKit graph)* - The graph of sampled nodes.
         """
-        self._check_graph(graph)
+        self._deploy_backend(graph)
         self._check_number_of_nodes(graph)
-        self._graph = graph
-        self._create_node_sets()
+        self._create_node_sets(graph)
         while len(self._sampled_nodes) < self.number_of_nodes:
             self._start_a_fire()
-        new_graph = self._graph.subgraph(self._sampled_nodes)
+        new_graph = self.backend.get_subgraph(graph, self._sampled_nodes)
         return new_graph
