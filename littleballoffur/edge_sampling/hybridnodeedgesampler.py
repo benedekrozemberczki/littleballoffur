d9acc3525171480701ef8f90914f9f64a2292fbe
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-09 11:11:36 +0000

    Backend based design

--- littleballoffur/edge_sampling/hybridnodeedgesampler.py
+++ littleballoffur/edge_sampling/hybridnodeedgesampler.py
@@ -1,6 +1,13 @@
 import random
 import networkx as nx
+import networkit as nk
+from typing import Union, List
 from littleballoffur.sampler import Sampler
+
+
+NKGraph = type(nk.graph.Graph())
+NXGraph = nx.classes.graph.Graph
+
 
 class HybridNodeEdgeSampler(Sampler):
     r"""An implementation of hybrid node-edge sampling. The algorithm alternates
