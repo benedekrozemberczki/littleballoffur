10b6b14e3a98faad0d104bb37bf309d8f325af5c
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-09 11:18:34 +0000

    Backend based design

--- littleballoffur/edge_sampling/hybridnodeedgesampler.py
+++ littleballoffur/edge_sampling/hybridnodeedgesampler.py
@@ -27,17 +27,17 @@ class HybridNodeEdgeSampler(Sampler):
         self._set_seed()
 
 
-    def _create_initial_edge_set(self):
+    def _create_initial_edge_set(self, graph):
         """
         Choosing initial edges.
         """
         self._sampled_edges = set()
-        edges = [edge for edge in self._graph.edges()]
+        edges = self.backend.get_edges(graph)
         while len(self._sampled_edges) < self.number_of_edges:
             score = random.uniform(0, 1)
             if score < self.p:
-                source_node = random.choice(range(self._graph.number_of_nodes()))
-                target_node = random.choice([node for node in self._graph.neighbors(source_node)])
+                source_node = random.choice(range(self.backend.get_number_of_nodes(graph)))
+                target_node = random.choice([node for node in self.backend.get_neighbors(graph, source_node)])
                 edge = sorted([source_node, target_node])
                 edge = tuple(edge)
             else:
@@ -55,9 +55,8 @@ class HybridNodeEdgeSampler(Sampler):
         Return types:
             * **new_graph** *(NetworkX or NetworKit graph)* - The graph of sampled nodes.
         """
-        self._check_graph(graph)
+        self._deploy_backend(graph)
         self._check_number_of_edges(graph)
-        self._graph = graph
-        self._create_initial_edge_set()
-        new_graph = nx.from_edgelist(self._sampled_edges)
+        self._create_initial_edge_set(graph)
+        new_graph = self.backend.graph_from_edgelist(self._sampled_edges)
         return new_graph
