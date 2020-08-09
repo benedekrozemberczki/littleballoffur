e401f6d7055e178bbfda9a4cd752f7fc834f67d2
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-09 22:50:24 +0000

    Backend dev

--- littleballoffur/exploration_sampling/randomwalkwithjumpsampler.py
+++ littleballoffur/exploration_sampling/randomwalkwithjumpsampler.py
@@ -1,6 +1,11 @@
 import random
 import networkx as nx
+import networkit as nk
+from typing import Union
 from littleballoffur.sampler import Sampler
+
+NKGraph = type(nk.graph.Graph())
+NXGraph = nx.classes.graph.Graph
 
 class RandomWalkWithJumpSampler(Sampler):
     r"""An implementation of node sampling by random walks with jumps.  The 
@@ -24,7 +29,7 @@ class RandomWalkWithJumpSampler(Sampler):
         """
         Choosing an initial node.
         """
-        self._current_node = random.choice(range(self._graph.number_of_nodes()))
+        self._current_node = random.choice(range(self.backend.get_number_of_nodes(graph)))
         self._sampled_nodes = set([self._current_node])
 
     def _do_a_step(self):
@@ -33,27 +38,25 @@ class RandomWalkWithJumpSampler(Sampler):
         """
         score = random.uniform(0, 1)
         if score < self.p:
-            self._current_node = random.choice(range(self._graph.number_of_nodes()))
+            self._current_node = random.choice(range(self.backend.get_number_of_nodes(graph)))
         else:
-            neighbors = self._graph.neighbors(self._current_node)
-            self._current_node = random.choice([neighbor for neighbor in neighbors])
+            self._current_node = self.backend.get_random_neighbor(graph, self._current_node)
         self._sampled_nodes.add(self._current_node)
 
-    def sample(self, graph: nx.classes.graph.Graph) -> nx.classes.graph.Graph:
+    def sample(self, graph: Union[NXGraph, NKGraph]) -> Union[NXGraph, NKGraph]:
         """
         Sampling nodes with a single random walk jumps.
 
         Arg types:
-            * **graph** *(NetworkX graph)* - The graph to be sampled from.
+            * **graph** *(NetworkX or NetworKit graph)* - The graph to be sampled from.
 
         Return types:
-            * **new_graph** *(NetworkX graph)* - The graph of sampled nodes.
+            * **new_graph** *(NetworkX or NetworKit graph)* - The graph of sampled nodes.
         """
-        self._check_graph(graph)
+        self._deploy_backend(graph)
         self._check_number_of_nodes(graph)
-        self._graph = graph
-        self._create_initial_node_set()
+        self._create_initial_node_set(graph)
         while len(self._sampled_nodes) < self.number_of_nodes:
-            self._do_a_step()
-        new_graph = self._graph.subgraph(self._sampled_nodes)
+            self._do_a_step(graph)
+        new_graph = self.backend.get_subgraph(graph, self._sampled_nodes)
         return new_graph
