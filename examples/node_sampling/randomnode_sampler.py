f5580b7cee06c5a478794c884ce3dd82e58687c6
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-05 14:19:39 +0000

    [EXAMPLES] Add dataset

--- examples/node_sampling/randomnode_sampler.py
+++ examples/node_sampling/randomnode_sampler.py
@@ -2,3 +2,11 @@
 
 from littleballoffur.node_sampling import RandomNodeSampler
 
+graph = nx.watts_strogatz_graph(1000, 10, 0)
+
+sampler = RandomNodeSampler()
+
+new_graph = sampler.sample(graph)
+
+
+
