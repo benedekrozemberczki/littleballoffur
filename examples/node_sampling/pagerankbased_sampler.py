7db0e7ca791a55644094fcbacf7f390b40741956
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-05 14:19:24 +0000

    [EXAMPLES] Add dataset

--- examples/node_sampling/pagerankbased_sampler.py
+++ examples/node_sampling/pagerankbased_sampler.py
@@ -1,5 +1,11 @@
 """Pagerank based sampler example."""
 
-
 from littleballoffur.node_sampling import PageRankBasedSampler
+
+graph = nx.watts_strogatz_graph(1000, 10, 0)
+
+sampler = PageRankBasedSampler()
+
+new_graph = sampler.sample(graph)
+
 
