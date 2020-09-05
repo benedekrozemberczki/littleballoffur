6331e9bd379fce51f92d92af1b9969722ee62284
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-05 13:05:56 +0000

    [EXAMPLES] Add dataset

--- examples/exploration_sampling/looperasedrandomwalk_sampler.py
+++ examples/exploration_sampling/looperasedrandomwalk_sampler.py
@@ -1,12 +1,12 @@
-"""Random walk with restart sampler example"""
+"""Loop erased random walk sampler example"""
 
 import networkx as nx
 
-from littleballoffur.exploration_sampling import RandomWalkWithRestartSampler
+from littleballoffur.exploration_sampling import LoopErasedRandomWalkSampler
 
 graph = nx.watts_strogatz_graph(1000, 10, 0)
 
-sampler = RandomWalkWithRestartSampler()
+sampler = LoopErasedRandomWalkSampler()
 
 new_graph = sampler.sample(graph)
 
