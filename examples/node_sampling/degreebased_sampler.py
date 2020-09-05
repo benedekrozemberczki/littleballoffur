3f16bdc6c5325ed796982af1e42b395b0719f219
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-05 14:19:02 +0000

    [EXAMPLES] Add dataset

--- examples/node_sampling/degreebased_sampler.py
+++ examples/node_sampling/degreebased_sampler.py
@@ -2,3 +2,10 @@
 
 from littleballoffur.node_sampling import DegreeBasedSampler
 
+graph = nx.watts_strogatz_graph(1000, 10, 0)
+
+sampler = DegreeBasedSampler()
+
+new_graph = sampler.sample(graph)
+
+
