52f670148133c52992c62f5e02ab9a85ec0bec6a
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-06 14:28:59 +0000

    [EXAMPLES] Add dataset

--- examples/exploration_sampling/forestfire_sampler.py
+++ examples/exploration_sampling/forestfire_sampler.py
@@ -1,12 +1,12 @@
-"""Loop erased random walk sampler example"""
+"""Forest fire sampler example"""
 
 import networkx as nx
 
-from littleballoffur.exploration_sampling import LoopErasedRandomWalkSampler
+from littleballoffur.exploration_sampling import ForestFire
 
 graph = nx.watts_strogatz_graph(1000, 10, 0)
 
-sampler = LoopErasedRandomWalkSampler()
+sampler = ForestFireSampler()
 
 new_graph = sampler.sample(graph)
 
