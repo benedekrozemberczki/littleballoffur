f43058d7d5cebba9c75b29a2b23c6febe0333aa3
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-07 22:48:27 +0000

    [EXAMPLES] Add class level examples.

--- examples/edge_sampling/randomnodeedge_sampler.py
+++ examples/edge_sampling/randomnodeedge_sampler.py
@@ -1,9 +1,9 @@
-"""Degree based sampler example."""
+"""Random node edge sampler example."""
 
-from littleballoffur.node_sampling import DegreeBasedSampler
+from littleballoffur.node_sampling import RandomNodeEdgeSampler
 
 graph = nx.watts_strogatz_graph(1000, 10, 0)
 
-sampler = DegreeBasedSampler()
+sampler = RandomNodeEdgeSampler()
 
 new_graph = sampler.sample(graph)
