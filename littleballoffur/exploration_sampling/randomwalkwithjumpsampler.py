8f62778351dc33f9d2eb88d29f534c252f99c4ce
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-08 14:12:56 +0000

    [Improve] Increase node indexing.

--- littleballoffur/exploration_sampling/randomwalkwithjumpsampler.py
+++ littleballoffur/exploration_sampling/randomwalkwithjumpsampler.py
@@ -58,7 +58,6 @@ class RandomWalkWithJumpSampler(Sampler):
             * **graph** *(NetworkX or NetworKit graph)* - The graph to be sampled from.
             * **start_node** *(int, optional)* - The start node.
 
-
         Return types:
             * **new_graph** *(NetworkX or NetworKit graph)* - The graph of sampled nodes.
         """
