5de93328343df1dbd3ec41747309f3b77625a7fa
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-11-28 19:50:50 +0000

    LBF

--- littleballoffur/node_sampling/__init__.py
+++ littleballoffur/node_sampling/__init__.py
@@ -2,3 +2,6 @@ from .randomnodesampler import RandomNodeSampler
 from .degreebasedsampler import DegreeBasedSampler
 from .pagerankbasedsampler import PageRankBasedSampler
 
+
+classes = ["RandomNodeSampler", "DegreeBasedSampler", "PageRankBasedSampler"]
+
