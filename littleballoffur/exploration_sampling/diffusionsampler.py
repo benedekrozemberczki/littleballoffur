108699de499ae0658d33166c8a9d2f47bb8789f3
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-12-01 21:42:54 +0000

    LBF

---
+++ littleballoffur/exploration_sampling/diffusionsampler.py
@@ -0,0 +1,64 @@
+import random
+import networkx as nx
+import networkit as nk
+from typing import Union
+from littleballoffur.sampler import Sampler
+
+
+NKGraph = type(nk.graph.Graph())
+NXGraph = nx.classes.graph.Graph
+
+
+class RandomWalkSampler(Sampler):
+    r"""An implementation of node sampling by random walks. A simple random walker
+    which creates an induced subgraph by walking around. `"For details about the
+    algorithm see this paper." <https://ieeexplore.ieee.org/document/5462078>`_
+
+    Args:
+        number_of_nodes (int): Number of nodes. Default is 100.
+        seed (int): Random seed. Default is 42.
+    """
+    def __init__(self, number_of_nodes: int=100, seed: int=42):
+        self.number_of_nodes = number_of_nodes
+        self.seed = seed
+        self._set_seed()
+
+    def _create_initial_node_set(self, graph, start_node):
+        """
+        Choosing an initial node.
+        """
+        if start_node is not None:
+            if start_node >= 0 and start_node < self.backend.get_number_of_nodes(graph):
+                self._current_node = start_node
+                self._sampled_nodes = set([self._current_node])
+            else:
+                raise ValueError("Starting node index is out of range.")
+        else:
+            self._current_node = random.choice(range(self.backend.get_number_of_nodes(graph)))
+            self._sampled_nodes = set([self._current_node])
+
+    def _do_a_step(self, graph):
+        """
+        Doing a single random walk step.
+        """
+        self._current_node = self.backend.get_random_neighbor(graph, self._current_node)
+        self._sampled_nodes.add(self._current_node)
+
+    def sample(self, graph: Union[NXGraph, NKGraph], start_node: int=None) -> Union[NXGraph, NKGraph]:
+        """
+        Sampling nodes with a single random walk.
+
+        Arg types:
+            * **graph** *(NetworkX or NetworKit graph)* - The graph to be sampled from.
+            * **start_node** *(int, optional)* - The start node.
+
+        Return types:
+            * **new_graph** *(NetworkX or NetworKit graph)* - The graph of sampled nodes.
+        """
+        self._deploy_backend(graph)
+        self._check_number_of_nodes(graph)
+        self._create_initial_node_set(graph, start_node)
+        while len(self._sampled_nodes) < self.number_of_nodes:
+            self._do_a_step(graph)
+        new_graph = self.backend.get_subgraph(graph, self._sampled_nodes)
+        return new_graph
