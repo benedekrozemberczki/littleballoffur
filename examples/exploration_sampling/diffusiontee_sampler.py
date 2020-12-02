2ec1207c918535858cb67305ff1ac2406290d66f
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-12-02 16:34:23 +0000

    LBF

---
+++ examples/exploration_sampling/diffusiontee_sampler.py
@@ -0,0 +1,13 @@
+"""Diffusion sampler example"""
+
+import networkx as nx
+
+from littleballoffur.exploration_sampling import DiffusionTreeSampler
+
+graph = nx.watts_strogatz_graph(1000, 10, 0)
+
+sampler = DiffusionTreeSampler()
+
+new_graph = sampler.sample(graph)
+
+
