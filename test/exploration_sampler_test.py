9bdc81ccbf274536870fcd84ba5105e10152ff1a
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-12-04 10:30:31 +0000

    LBF

--- test/exploration_sampler_test.py
+++ test/exploration_sampler_test.py
@@ -246,6 +246,17 @@ def test_depth_first_search_sampler():
     assert 1 == nk.components.ConnectedComponents(new_graph).run().numberOfComponents()
     assert type(new_graph) == NKGraph
 
+    sampler = DepthFirstSearchSampler(number_of_nodes=1)
+
+    graph = nk.nxadapter.nx2nk(nx.watts_strogatz_graph(200, 10, 0))
+
+    new_graph = sampler.sample(graph)
+
+    assert new_graph.numberOfEdges()+1 == new_graph.numberOfNodes()
+    assert sampler.number_of_nodes == new_graph.numberOfNodes()
+    assert 1 == nk.components.ConnectedComponents(new_graph).run().numberOfComponents()
+    assert type(new_graph) == NKGraph
+
 
 
 #----------------------------------------#
