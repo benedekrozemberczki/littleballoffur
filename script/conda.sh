3edbab7084e66b89d3d34ddf97c12d5d8d26f51a
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-12-11 15:06:13 +0000

    Fix actions.

--- script/conda.sh
+++ script/conda.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-  wget -nv https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-  chmod +x miniconda.sh
-  ./miniconda.sh -b
-  PATH=/home/travis/miniconda3/bin:${PATH}
-fi
 
+wget -nv https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+chmod +x miniconda.sh
+./miniconda.sh -b
 conda update --yes conda
-
-conda create --yes -n test python="${PYTHON_VERSION}"
+conda create --yes -n test
