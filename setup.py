d6bae5e7df970294428ccc8132bcdf28e856452a
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-08-17 20:57:17 +0100

    LBF

--- setup.py
+++ setup.py
@@ -3,18 +3,20 @@ from setuptools import find_packages, setup
 
 on_rtd = os.environ.get("READTHEDOCS") == "True"
 
-install_requires = ["decorator==4.4.2",
-                    "cmake",
-                    "Cython",
-                    "networkx==2.4",
-                    "tqdm",
-                    "python-louvain",
-                    "scikit-network",
-                    "pandas",
-                    "numpy",
-                    "six",
-                    "scipy",
-                    "networkx==2.4"]
+install_requires = [
+    "decorator==4.4.2",
+    "cmake",
+    "Cython",
+    "networkx==2.4",
+    "tqdm",
+    "python-louvain",
+    "scikit-network",
+    "pandas",
+    "numpy",
+    "six",
+    "scipy",
+    "networkx==2.4",
+]
 
 if not on_rtd:
     install_requires.append("networkit==7.1")
@@ -23,38 +25,42 @@ setup_requires = ["cython", "numpy", "pytest-runner"]
 
 tests_require = ["pytest", "pytest-cov", "mock"]
 
-keywords = ["community",
-            "detection",
-            "networkx",
-            "graph",
-            "snow ball",
-            "metropolis hastings",
-            "shortest path",
-            "loop erased",
-            "spiky ball",
-            "graph sampling",
-            "sampling",
-            "tree sampling",
-            "random walk",
-            "forest fire"]
+keywords = [
+    "community",
+    "detection",
+    "networkx",
+    "graph",
+    "snow ball",
+    "metropolis hastings",
+    "shortest path",
+    "loop erased",
+    "spiky ball",
+    "graph sampling",
+    "sampling",
+    "tree sampling",
+    "random walk",
+    "forest fire",
+]
 
 setup(
-  name = "littleballoffur",
-  version = "2.1.8",
-  license = "MIT",
-  description = "A general purpose library for subsampling graphs.",
-  author = "Benedek Rozemberczki",
-  author_email = "benedek.rozemberczki@gmail.com",
-  url = "https://github.com/benedekrozemberczki/littleballoffur",
-  download_url = "https://github.com/benedekrozemberczki/littleballoffur/archive/v_20008.tar.gz",
-  keywords = keywords,
-  install_requires = install_requires,
-  setup_requires = setup_requires,
-  tests_require = tests_require,
-  packages = find_packages(),
-  classifiers = ["Development Status :: 3 - Alpha",
-                 "Intended Audience :: Developers",
-                 "Topic :: Software Development :: Build Tools",
-                 "License :: OSI Approved :: MIT License",
-                 "Programming Language :: Python :: 3.7"],
+    name="littleballoffur",
+    version="2.1.8",
+    license="MIT",
+    description="A general purpose library for subsampling graphs.",
+    author="Benedek Rozemberczki",
+    author_email="benedek.rozemberczki@gmail.com",
+    url="https://github.com/benedekrozemberczki/littleballoffur",
+    download_url="https://github.com/benedekrozemberczki/littleballoffur/archive/v_20008.tar.gz",
+    keywords=keywords,
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
