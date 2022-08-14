5390febaa4bc3f27130e88026f0a51c0fcdf75c9
Benedek Rozemberczki <kmdb028@astrazeneca.net>
2022-08-14 13:27:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@ from setuptools import find_packages, setup
 
 on_rtd = os.environ.get("READTHEDOCS") == "True"
 
-install_requires = ["numpy==1.23.0",
-                    "networkx==2.6.0",
+install_requires = ["numpy<1.23.1",
+                    "networkx<2.7",
                     "decorator==4.4.2",
                     "cmake",
                     "Cython",
                     "tqdm",
                     "python-louvain",
-                    "pandas",
-                    "numpy",
+                    "pandas<=1.3.5",
                     "six",
                     "scipy"]
 
