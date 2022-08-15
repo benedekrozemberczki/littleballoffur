6953a48e2578bb0cc9642a5b5f78ddf4d4899515
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2022-08-15 09:42:30 +0100

    LBF

--- littleballoffur/__init__.py
+++ littleballoffur/__init__.py
@@ -5,6 +5,8 @@ from littleballoffur.backend import NetworKitBackEnd, NetworkXBackEnd
 from littleballoffur.node_sampling import *
 from littleballoffur.edge_sampling import *
 from littleballoffur.exploration_sampling import *
+from littleballoffur.version import __version__  # noqa:F401,F403
+
 
 __all__ = [
     "littleballoffur",
